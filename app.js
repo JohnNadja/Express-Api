ea0f4a40ec760a334f022d47f0f7517300efaecd
JohnNadja <cv3802@innovaccion.mx>
2022-04-24 14:30:40 -0500

    5: Creating endpoint GET

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ const port = 3000
 
 // Con esto inicializamos esta app
 app.listen(port, () => {
- console.log(`Example app listening on port ${port}`)
+ console.log(`Example app listening on port ${port}. Ready to go`)
+})
+
+app.get('/v1/explorers', (req, res) =>{
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id: 1, name: "John1"}
+    const explorer2 = {id: 2, name: "John2"}
+    const explorer3 = {id: 3, name: "John3"}
+    const explorer4 = {id: 4, name: "John4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
 })
